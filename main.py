de66b1df0eb16c21ab243317a632132b88fcf026
gk <gk@xyz.de>
2014-04-13 13:22:59 +0200

    1st commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+accords = dict()
+accords[(1,5,8)] = ""
+accords[(1,5,8,12)] = "maj7"
+accords[(1,5,8,11)] = "7"
+accords[(1,4,8,12)] = "mmaj7"
+accords[(1,4,8,11)] = "m7"
+accords[(1,4,8)] = "m"
+accords[(1,4,8,12)] = "mmaj7"
+accords[(1,4,8,11)] = "m7"
